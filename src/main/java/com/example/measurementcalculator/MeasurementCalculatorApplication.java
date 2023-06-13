5b6961947a66c505d62a010b232f584fcd8979a8
Enes <enesozturkk10@gmail.com>
2023-06-13 02:40:34 +0200

    Initial commit

---
+++ src/main/java/com/example/measurementcalculator/MeasurementCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.measurementcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasurementCalculatorApplication.class, args);
+    }
+
+}
